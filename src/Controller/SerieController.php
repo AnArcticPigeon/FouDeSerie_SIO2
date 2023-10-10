01f960d220cf47386e97e8c00101c983c25d51d9
Adrien <an.arctic.pigeon@gmail.com>
2023-10-10 15:38:46 +0200

    Affichage liste des series + detail d'unse serie

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,19 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
 use App\Services\SerieService;
 use App\Services\PaysService;
+use DateTime;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SerieController extends AbstractController
 {
-    #[Route('/series', name: 'app_series')]
-    public function index(SerieService $a): Response
+    #[Route('/testEntity', name: 'app_series_test')]
+    public function testEntity(ManagerRegistry $doctrine)
     {
-        $lesSeries = $a->getSeries();
-        $countSeries = $a->countSeries();
+        // Instanciation classique d'un objet : création d’une entité
+        $serie = new Serie();
+        $serie->setTitre('PingKungFu');
+        $serie->setResume('Un Pinguin en quéte de vengeance part pour la chine avec l espoire d aprendre le kingfu afin de vanger son frére.');
+        $serie->setPremiereDiffusion(new DateTime("06-12-2015"));
+        $serie->setNbEpisodes(14);
+        $serie->setImage('https://ibb.co/y0RtpDG');
+        $entityManager=$doctrine->getManager();
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($serie);
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+        return new Response('Saved new product with id '.$serie->getId());
+    }
+
+    #[Route('/series', name: 'app_series')]
+    public function index(ManagerRegistry $doctrine)
+    {
+        /*Cette instruction renvoie une instance de la classe ProductRepository; Le
+        lien a été fait grace à l'attribut PHP #[ORM\Entity(repositoryClass:…) dans
+        la classe Product */
+        $repository = $doctrine->getRepository(Serie::class);
+        // look for *all* Product objects
+        $lesSeries = $repository->findBy([],['titre' => 'ASC']);
+
+
+        $countSeries = count($lesSeries);
         return $this->render('serie/index.html.twig', [
             'controller_name' => 'SerieController',
             'lesSeries' => $lesSeries,
@@ -23,12 +51,14 @@ class SerieController extends AbstractController
     }
 
     #[Route('/series/{id}', name: 'app_series_detail')]
-    public function Detail(SerieService $a, $id, PaysService $paysService): Response
+    public function Detail($id, ManagerRegistry $doctrine)
     {
-        $laSerie = $a->getUneSerie($id, $paysService);
+        $repository = $doctrine->getRepository(Serie::class);
+        $laSerie = $repository->find($id);
         return $this->render('serie/detail.html.twig', [
             'controller_name' => 'SerieController',
             'uneSerie' => $laSerie,
         ]);
     }
+
 }
---
+++ src/Entity/Serie.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SerieRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SerieRepository::class)]
+class Serie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $resume = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true, name: 'premiereDiffusion')]
+    private ?\DateTimeInterface $premiereDiffusion = null;
+
+    #[ORM\Column(nullable: true, name: 'nbEpisodes')]
+    private ?int $nbEpisodes = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getResume(): ?string
+    {
+        return $this->resume;
+    }
+
+    public function setResume(?string $resume): static
+    {
+        $this->resume = $resume;
+
+        return $this;
+    }
+
+    public function getPremiereDiffusion(): ?\DateTimeInterface
+    {
+        return $this->premiereDiffusion;
+    }
+
+    public function setPremiereDiffusion(?\DateTimeInterface $premiereDiffusion): static
+    {
+        $this->premiereDiffusion = $premiereDiffusion;
+
+        return $this;
+    }
+
+    public function getNbEpisodes(): ?int
+    {
+        return $this->nbEpisodes;
+    }
+
+    public function setNbEpisodes(?int $nbEpisodes): static
+    {
+        $this->nbEpisodes = $nbEpisodes;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
